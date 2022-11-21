54ff3b0080cb2f05bd6b681afc25adca712785b4
min33sky <min33sky@naver.com>
2022-11-21 12:54:01 +0900

    feat: move - scroll and button

--- src/components/DraggableTabs.tsx
+++ src/components/DraggableTabs.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 
 interface Props {
@@ -7,41 +7,85 @@ interface Props {
 
 export default function DraggableTabs({ tabItems }: Props) {
   const [isDragging, setIsDragging] = useState(false);
+  const scrollRef = useRef<HTMLUListElement>(null);
 
   const handleDragStart = (e: React.MouseEvent<HTMLUListElement>) => {
     setIsDragging(true);
+  };
+
+  const handleDragging = (e: React.MouseEvent<HTMLUListElement>) => {
+    if (isDragging) {
+      const ul = e.currentTarget;
+      const li = e.target as HTMLLIElement;
+      const x = e.clientX;
+      console.log('move', e.movementX, e.pageX, e.clientX);
+      scrollRef.current?.scrollTo({
+        left: scrollRef.current.scrollLeft + e.movementX,
+      });
+    }
   };
 
   const handleDragEnd = (e: React.MouseEvent<HTMLUListElement>) => {
     setIsDragging(false);
   };
 
+  const handleClick = (e: React.MouseEvent) => {
+    const target = e.currentTarget;
+    if (target.id === 'leftBtn') {
+      scrollRef.current?.scrollTo({
+        left: scrollRef.current.scrollLeft - 350,
+        behavior: 'smooth',
+      });
+    } else if (target.id === 'rightBtn') {
+      scrollRef.current?.scrollTo({
+        left: scrollRef.current.scrollLeft + 350,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
-    <div className="relative w-10/12 max-w-xl rounded-md bg-slate-200 px-3 py-5 shadow-lg">
-      <button>
-        <ArrowLeftIcon className="h-4 w-4" />
-      </button>
+    <div className="relative w-10/12 max-w-xl rounded-md bg-slate-200 px-10 py-5 shadow-lg">
+      <div className="pointer-events-none absolute top-0  left-4 flex h-full w-32 items-center bg-gradient-to-r from-slate-200">
+        <button
+          id="leftBtn"
+          onClick={handleClick}
+          className="rounded-full p-2 transition hover:bg-slate-300/50"
+        >
+          <ArrowLeftIcon className="pointer-events-auto h-5 w-5" />
+        </button>
+      </div>
 
       <ul
         aria-label="tabs-box"
-        className="flex gap-3 overflow-x-hidden scroll-smooth whitespace-nowrap"
+        ref={scrollRef}
+        className={`flex items-center gap-3 overflow-x-hidden whitespace-nowrap  ${
+          isDragging ? 'cursor-grabbing ' : 'cursor-grab '
+        }`}
         onMouseDown={handleDragStart}
+        onMouseMove={handleDragging}
         onMouseUp={handleDragEnd}
         onMouseLeave={handleDragEnd}
       >
         {tabItems.map((tabItem, index) => (
           <li
             key={index}
-            className="flex h-12 w-32 select-none items-center justify-center rounded-md bg-slate-100"
+            className="pointer-events-none flex h-12 w-32 select-none items-center justify-center rounded-md bg-slate-100 px-2"
           >
             {tabItem}
           </li>
         ))}
       </ul>
 
-      <button>
-        <ArrowRightIcon className="h-4 w-4" />
-      </button>
+      <div className="pointer-events-none absolute top-0 right-4 flex h-full w-32 items-center justify-end bg-gradient-to-l from-slate-200">
+        <button
+          id="rightBtn"
+          onClick={handleClick}
+          className="rounded-full p-2 transition hover:bg-slate-300/50"
+        >
+          <ArrowRightIcon className="pointer-events-auto h-5 w-5" />
+        </button>
+      </div>
     </div>
   );
 }
